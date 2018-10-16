4fc137bc70b5f2f91f6b5548ded70aa5caf1a759
Ngela <ngela@Ngela-Pat-Yu.local>
2018-10-16 05:06:24 +0200

    app/app.component.*-Edit the App component  and get the app-error(global component)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        CoreModule
       ],
       declarations: [
         AppComponent
@@ -26,10 +28,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('AngularPullRequests');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AngularPullRequests!');
-  });
 });
